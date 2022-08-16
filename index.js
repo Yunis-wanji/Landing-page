72b5210e45f3c9432c19bb4c8964d41e415e1bc9
Yunis-wanji <94746081+Yunis-wanji@users.noreply.github.com>
2022-08-16 20:46:01 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// function sumArrays(arr){
+//     let sum = 0
+//     for(let i =0; i<arr.length; i++){
+//         sum = sum + arr[i];
+//     }
+//     return sum
+// }
+// const numbers = [1,2,3,4,5]
+// console.log(sumArrays(numbers))
+
+
+
+// function userNames(name){
+//     let herName = []
+// for(let i =0; i<name.length; i++){
+//     herName = name
+// }
+// return herName;
+// }
+// let names = ['Eunice', 'Wanjiru', 'Kimani', 'Shiru']
+// console.log(userNames(names))
+
+
+// const sumAllNum = (...arg)=> {
+//     let sum = 0
+//     for(const element of arg){
+//         sum += element
+//     }
+//     return sum
+// }
+// console.log(sumAllNum(1,2,3,4,5,6))
